5148a56e58af94997327587cc8d63d09debb2e80
Quan Dou <qd9zb@virginia.edu>
2022-11-30 00:46:08 -0500

    finish Project 1e

---
+++ proj1/gh2/GuitarHero.java
@@ -0,0 +1,46 @@
+package gh2;
+import edu.princeton.cs.algs4.StdAudio;
+import edu.princeton.cs.algs4.StdDraw;
+
+/**
+ * A client that uses the synthesizer package to replicate a plucked guitar string sound
+ */
+public class GuitarHero {
+    public static final double CONCERT_A = 440.0;
+
+    public static void main(String[] args) {
+        String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+        GuitarString[] strings = new GuitarString[37];
+        for (int i = 0; i < 37; i++) {
+            double frequency = CONCERT_A * Math.pow(2, (i - 24.0) / 12.0);
+            strings[i] = new GuitarString(frequency);
+        }
+
+        while (true) {
+
+            /* check if the user has typed a key; if so, process it */
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                int index = keyboard.indexOf(key);
+                if (index != -1) {
+                    strings[keyboard.indexOf(key)].pluck();
+                }
+            }
+
+            /* compute the superposition of samples */
+            double sample = 0.0;
+            for (int i = 0; i < 37; i++) {
+                sample += strings[i].sample();
+            }
+
+            /* play the sample on standard audio */
+            StdAudio.play(sample);
+
+            /* advance the simulation of each guitar string by one step */
+            for (int i = 0; i < 37; i++) {
+                strings[i].tic();
+            }
+        }
+    }
+}
+
